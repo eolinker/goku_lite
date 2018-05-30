c05f2fc94823953eb37c961cc123733d4bf95afa
eoLinker API Management <dev@eolinker.com>
2018-05-30 10:50:17 +0800

    V2.1.3

--- source_code_frontend/src/app/directive/directive.module.js
+++
@@ -1,9 +0,0 @@
-(function() {
-    'use strict';
-    /*
-     * author：广州银云信息科技有限公司
-     * 定义指令模块js
-     */
-    angular.module('goku.directive', [])
-
-})();
--- source_code_frontend/src/app/filter/filter.module.js
+++
@@ -1,8 +0,0 @@
-(function() {
-    'use strict';
-    /*
-    * author:广州银云信息科技有限公司
-    * 过滤器模块
-    */
-    angular.module('goku.filter', [])
-})();
